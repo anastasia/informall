f50843a9ee1f5e3bcd4e53d82b2adc7644075595
Anastasia <aaizman@HLSAdmins-MacBook-Pro.local>
2017-02-25 20:13:46 -0500

    add - parsing for kml

--- libraries/resources.py
+++ libraries/resources.py
@@ -1,6 +1,7 @@
 import csv
 import json
-from .models import *
+import simplekml
+from libraries.models import *
 
 def get_libraries(libraries_csv):
     with open(libraries_csv, 'rb') as csvfile:
@@ -25,16 +26,35 @@ def get_news(news_json):
     for state_name in news_data:
         state, created = State.objects.get_or_create(name=state_name)
         state.save()
-        all_news = news_data[state_name]['newspaper']
-        for n in all_news:
-            try:
-                news, created = News.objects.get_or_create(
-                    newstype=n['media-class'],
-                    name=n['name'],
-                    state=state,
-                    longitude=n['city-county-long'],
-                    latitude=n['city-county-lat'],)
-                news.save()
-            except:
-                print("ERROR!", n)
-                pass
+        state_news = [news_data[state_name]['newspaper'], news_data[state_name]['tv'], news_data[state_name]['radio'], ]
+        for type_of_news in state_news:
+            for n in type_of_news:
+                try:
+                    news, created = News.objects.get_or_create(
+                        newstype=n['media-class'],
+                        name=n['name'],
+                        state=state,
+                        longitude=n['city-county-long'],
+                        latitude=n['city-county-lat'],)
+                    news.save()
+                except:
+                    print("ERROR!", n)
+                    pass
+
+
+def convert_to_kml(kml_sheet, type_of_org, descriptor_of_org):
+    """
+        type_of_org = library | news
+        descriptor_of_org = public | academic | private | television | newspaper | radio
+    """
+    if type_of_org == 'library':
+        all_orgs = list(Library.objects.all())
+    elif type_of_org == 'news':
+        all_orgs = list(News.objects.filter(newstype=descriptor_of_org))
+
+    kml = simplekml.Kml()
+    for org in all_orgs:
+        kml.newpoint(name=org.name, description=descriptor_of_org, coords=[(org.latitude, org.longitude)])
+
+    with open(kml_sheet, 'rb+') as f:
+        f.write(kml.kml())
